37de9763a1a28cf5fd4edb441f317bc303c08338
CrystalTime <rmmincraft1.4@gmail.com>
2025-12-14 10:51:13 +0100

    optimise download

--- PycharmProjects/Working/chess/src/data/chess_images/pieces_downloader.py
+++ PycharmProjects/Working/chess/src/data/chess_images/pieces_downloader.py
@@ -19,18 +19,25 @@ pieces = {
 }
 
 base_url = "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/"
+path = f"{os.path.dirname(os.path.abspath(__file__))}"
+files_to_download: list[str] = []
 
-for code, name in pieces.items():
+for key in pieces.keys():
+    tmp_path = f"{path}/{key}.png"
+    if not os.path.exists(tmp_path):
+        files_to_download.append(key)
+    else:
+        print("Already exist :", tmp_path[-34:])
+
+for code in files_to_download:
     url = f"{base_url}{code}.png"
     response = requests.get(url)
     if response.status_code == 200:
-        path = f"{os.path.dirname(os.path.abspath(__file__))}/{code}.png"  # Enregistre directement dans le répertoire courant
-        if not os.path.exists(path):
-            with open(path, "wb") as f:
-                f.write(response.content)
-            print(f"Téléchargé et enregistré : {path}")
-        else:
-            print(f"Already exist : {path[-34:]}")
+        # Enregistre directement dans le répertoire courant
+        code_path = f"{path}/{code}.png"
+        with open(code_path, "wb") as f:
+            f.write(response.content)
+        print(f"Téléchargé et enregistré : {path}")
     else:
         print(f"Erreur téléchargement : {url}")
 else:
